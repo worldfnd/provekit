e40ac917775041a5c24f23411a9cb7c9d51eb359
Piotr Heilman <piotr.heilman@toolsforhumanity.com>
2025-07-25 11:32:03 +0200

    test

--- noir-r1cs/src/cli/main.rs
+++ noir-r1cs/src/cli/main.rs
@@ -1,39 +1,15 @@
 #![allow(missing_docs)]
 mod cmd;
-mod measuring_alloc;
-mod span_stats;
 
 use {
-    self::{cmd::Command, measuring_alloc::MeasuringAllocator, span_stats::SpanStats},
-    crate::measuring_alloc::MeasuringAllocatorState,
+    self::cmd::Command,
     anyhow::Result,
     std::{clone::Clone, convert::Into},
-    tracing::subscriber,
-    tracing_subscriber::{self, layer::SubscriberExt as _, Layer, Registry},
+    tracing_subscriber::{self, layer::SubscriberExt as _, Layer},
 };
 
-static ALLOC_STATE: MeasuringAllocatorState = MeasuringAllocatorState::new();
-
-#[global_allocator]
-static ALLOC_TRACY: tracy_client::ProfiledAllocator<MeasuringAllocator> =
-    tracy_client::ProfiledAllocator::new(MeasuringAllocator::new(&ALLOC_STATE), 100);
-
 fn main() -> Result<()> {
-    let subscriber = Registry::default()
-        .with(tracing_tracy::TracyLayer::default())
-        .with(SpanStats);
-
-    subscriber::set_global_default(subscriber)?;
-
-    let _client = tracy_client::Client::start();
-
     // Run CLI command
     let args = argh::from_env::<cmd::Args>();
-    let res = args.run();
-
-    unsafe {
-        tracy_client_sys::___tracy_shutdown_profiler();
-    }
-
-    res
+    args.run()
 }
