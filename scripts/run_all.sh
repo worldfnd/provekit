636ffd4e92a393c2dc3b528f9a7188d7f3430abd
wizard <112275929+famouswizard@users.noreply.github.com>
2025-09-12 12:57:07 +0300

    Fix: missing file handling and speed up execution

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,6 +1,8 @@
 # Run this in the `noir-r1cs` directory!
 
+shopt -s nullglob
+cargo build --release --bin circuit_stats
 for file in noir-passport-examples/*.json; do
   echo "$file"
-  cargo run --bin circuit_stats -- "$file" noir-examples/basic/target/basic.gz
+  ./target/release/circuit_stats "$file" noir-examples/basic/target/basic.gz
 done
