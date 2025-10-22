6840b5a6d88f7051a126672151e68629b061a93a
Xander van der Goot <xandervandergoot@gmail.com>
2025-10-22 12:13:22 +0800

    NTT: cache roots global

--- ntt/src/main.rs
+++ ntt/src/main.rs
@@ -1,7 +1,7 @@
 /// Executable for profiling NTT
 use {
     ark_bn254::Fr,
-    ntt::{NTTEngine, NTT},
+    ntt::{ntt_nr, NTTEngine, NTT},
     std::hint::black_box,
 };
 
@@ -9,7 +9,8 @@ fn main() {
     rayon::ThreadPoolBuilder::new().build_global().unwrap();
 
     let mut input = NTT::new(vec![Fr::from(1); 2_usize.pow(24)], 1).unwrap();
-    let mut engine = NTTEngine::with_order(input.order());
-    engine.ntt_nr(&mut input);
+    // TODO how to preinitialise the NTT?
+    // let mut engine = NTTEngine::with_order(input.order());
+    ntt_nr(&mut input);
     black_box(input);
 }
