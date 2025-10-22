b000b48d0491b628251fe1eb0fd81b32a05f8636
Xander van der Goot <xandervandergoot@gmail.com>
2025-10-22 15:56:56 +0800

    fixup! fixups

--- ntt/src/main.rs
+++ ntt/src/main.rs
@@ -9,8 +9,6 @@ fn main() {
     rayon::ThreadPoolBuilder::new().build_global().unwrap();
 
     let mut input = NTT::new(vec![Fr::from(1); 2_usize.pow(24)], 1).unwrap();
-    // TODO how to preinitialise the NTT?
-    // let mut engine = NTTEngine::with_order(input.order());
     ntt_nr(&mut input);
     black_box(input);
 }
