dd07496ce39225d9831a1139d0eccf432010c103
Xander van der Goot <xandervandergoot@gmail.com>
2026-03-18 14:37:08 +0800

    RS: extend bench

--- provekit/common/benches/ntt.rs
+++ provekit/common/benches/ntt.rs
@@ -14,51 +14,61 @@ fn main() {
 // (log2 of per-polynomial NTT size, number of polynomials)
 const NTT_CASES: &[(u32, usize)] = &[(22, 1), (24, 1)];
 
-const RS_CASES: &[(u32, usize, usize)] = &[
-    (16, 2, 2),
-    (18, 2, 2),
-    (20, 2, 3),
-    (16, 4, 3),
-    (18, 4, 3),
-    (20, 4, 4),
-    (22, 4, 4),
+const RS_CASES: &[(u32, usize, usize, usize)] = &[
+    (16, 2, 2, 1),   (16, 2, 2, 128),
+    (18, 2, 2, 1),   (18, 2, 2, 128),
+    (20, 2, 3, 1),   (20, 2, 3, 128),
+    (16, 4, 3, 1),   (16, 4, 3, 128),
+    (18, 4, 3, 1),   (18, 4, 3, 128),
+    (20, 4, 4, 1),   (20, 4, 4, 128),
+    (22, 4, 4, 1),   (22, 4, 4, 128),
 ];
 
 #[divan::bench(args = RS_CASES)]
-fn provekit_rs(bencher: Bencher, case: &(u32, usize, usize)) {
+fn provekit_rs(bencher: Bencher, case: &(u32, usize, usize, usize)) {
     let ntt = InPlaceNTT::<FieldElement>::default();
     bencher
         .with_inputs(|| {
-            let (exp, expansion, coset_sz) = *case;
+            let (exp, expansion, coset_sz, num_polys) = *case;
             let mut rng = ark_std::test_rng();
-            let size = 1 << exp;
-            let coeffs: Vec<_> = (0..size).map(|_| FieldElement::rand(&mut rng)).collect();
-            (coeffs, expansion, coset_sz)
+            let total_size = 1 << exp;
+            let poly_size = total_size / num_polys;
+            let polys: Vec<Vec<FieldElement>> = (0..num_polys)
+                .map(|_| (0..poly_size).map(|_| FieldElement::rand(&mut rng)).collect())
+                .collect();
+            (polys, expansion, coset_sz)
         })
-        .bench_values(|(coeffs, expansion, coset_sz)| {
+        .bench_values(|(polys, expansion, coset_sz)| {
+            let poly_refs: Vec<&[FieldElement]> = polys.iter().map(|p| p.as_slice()).collect();
+            let poly_size = poly_refs[0].len();
             black_box(ntt.interleaved_encode(
-                &[&coeffs],
-                (coeffs.len() >> coset_sz) * expansion,
+                &poly_refs,
+                (poly_size >> coset_sz) * expansion,
                 1 << coset_sz,
             ))
         });
 }
 
 #[divan::bench(args = RS_CASES)]
-fn ark_rs(bencher: Bencher, case: &(u32, usize, usize)) {
+fn ark_rs(bencher: Bencher, case: &(u32, usize, usize, usize)) {
     let ntt = ArkNtt::<FieldElement>::default();
     bencher
         .with_inputs(|| {
-            let (exp, expansion, coset_sz) = *case;
+            let (exp, expansion, coset_sz, num_polys) = *case;
             let mut rng = ark_std::test_rng();
-            let size = 1 << exp;
-            let coeffs: Vec<_> = (0..size).map(|_| FieldElement::rand(&mut rng)).collect();
-            (coeffs, expansion, coset_sz)
+            let total_size = 1 << exp;
+            let poly_size = total_size / num_polys;
+            let polys: Vec<Vec<FieldElement>> = (0..num_polys)
+                .map(|_| (0..poly_size).map(|_| FieldElement::rand(&mut rng)).collect())
+                .collect();
+            (polys, expansion, coset_sz)
         })
-        .bench_values(|(coeffs, expansion, coset_sz)| {
+        .bench_values(|(polys, expansion, coset_sz)| {
+            let poly_refs: Vec<&[FieldElement]> = polys.iter().map(|p| p.as_slice()).collect();
+            let poly_size = poly_refs[0].len();
             black_box(ntt.interleaved_encode(
-                &[&coeffs],
-                (coeffs.len() >> coset_sz) * expansion,
+                &poly_refs,
+                (poly_size >> coset_sz) * expansion,
                 1 << coset_sz,
             ))
         });
