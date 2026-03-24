36d3eb7fd0806cdc20d73312602c5054e791be81
Rose Jethani <rosejethani@Roses-MacBook-Pro.local>
2026-03-24 17:56:00 +0530

    style: Clean up formatting in witness module imports

--- provekit/common/src/witness/mod.rs
+++ provekit/common/src/witness/mod.rs
@@ -20,9 +20,8 @@ pub use {
     limbs::{Limbs, MAX_LIMBS},
     ram::{SpiceMemoryOperation, SpiceWitnesses},
     scheduling::{
-        
-        DependencyInfo, Layer, LayerScheduler, LayerType, LayeredWitnessBuilders, SplitError, SplitWitnessBuilders,
-    ,
+        DependencyInfo, Layer, LayerScheduler, LayerType, LayeredWitnessBuilders, SplitError,
+        SplitWitnessBuilders,
     },
     witness_builder::{
         CombinedTableEntryInverseData, ConstantTerm, NonNativeEcOp, ProductLinearTerm, SumTerm,
