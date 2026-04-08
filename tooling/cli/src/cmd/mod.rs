c066a64113245ca27b50260d00a022eeb4828d7e
x-senpai-x <sharmautsav0531@gmail.com>
2026-04-08 22:21:32 +0530

    initial verifier attempt

--- tooling/cli/src/cmd/mod.rs
+++ tooling/cli/src/cmd/mod.rs
@@ -1,6 +1,7 @@
 mod analyze_pkp;
 mod circuit_stats;
 mod generate_gnark_inputs;
+mod generate_noir_inputs;
 mod prepare;
 mod prove;
 mod show_inputs;
@@ -44,6 +45,7 @@ enum Commands {
     CircuitStats(circuit_stats::Args),
     Verify(verify::Args),
     GenerateGnarkInputs(generate_gnark_inputs::Args),
+    GenerateNoirInputs(generate_noir_inputs::Args),
     ShowInputs(show_inputs::Args),
 }
 
@@ -62,6 +64,7 @@ impl Command for Commands {
             Self::CircuitStats(args) => args.run(),
             Self::Verify(args) => args.run(),
             Self::GenerateGnarkInputs(args) => args.run(),
+            Self::GenerateNoirInputs(args) => args.run(),
             Self::ShowInputs(args) => args.run(),
         }
     }
