f8d737149dcd7320477f07b627b082fcf04a1e0d
Xander van der Goot <xandervandergoot@gmail.com>
2026-04-10 18:31:51 +0800

    NTT: add roots method

--- ntt/src/ark_interleaved.rs
+++ ntt/src/ark_interleaved.rs
@@ -7,7 +7,7 @@ define_ntt!(interleaved_ntt_nr, Fr, ark_kernel);
 
 pub fn ntt_nr_ark(values: &mut [Fr], codeword_size: usize, num_groups: usize) {
     let new_root = extend_roots_table(codeword_size);
-    interleaved_ntt_nr(&new_root.0, values, codeword_size, num_groups)
+    interleaved_ntt_nr(new_root.roots(), values, codeword_size, num_groups)
 }
 
 #[inline(always)]
